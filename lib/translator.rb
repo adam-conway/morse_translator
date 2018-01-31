e7325c3b68b7c76a67e1476693b654fc38a4d9fb
adam-conway <adam.n.conway@gmail.com>
2018-01-31 10:50:49 -0700

    Improved test coverage

--- lib/translator.rb
+++ lib/translator.rb
@@ -81,5 +81,3 @@ class Translator
   end
 
 end
-
-binding.pry
--- test/translator_test.rb
+++ test/translator_test.rb
@@ -6,37 +6,35 @@ require 'pry'
 
 class TranslatorTest < Minitest::Test
   def setup
+    @translator = Translator.new
   end
 
   def test_translator_class_initalizes
-    translator = Translator.new
-    assert_equal "c", translator.dictionary.keys[2]
-    assert_equal " ", translator.dictionary.keys[-1]
-    assert_equal "-----", translator.dictionary.values[-2]
-    assert_equal "-...", translator.dictionary.values[1]
+    assert_equal "c", @translator.dictionary.keys[2]
+    assert_equal " ", @translator.dictionary.keys[-1]
+    assert_equal "-----", @translator.dictionary.values[-2]
+    assert_equal "-...", @translator.dictionary.values[1]
   end
 
   def test_eng_to_morse
-    translator = Translator.new
-    assert_equal "......-...--.", translator.eng_to_morse("help")
-    assert_equal "-. ...-.----", translator.eng_to_morse("te st1")
+    assert_equal "......-...--.", @translator.eng_to_morse("help")
+    assert_equal "-. ...-.----", @translator.eng_to_morse("te st1")
   end
 
   def test_downcase
-    translator = Translator.new
-    assert_equal "......-...--.", translator.eng_to_morse("heLP")
-    assert_equal "-....-...--", translator.eng_to_morse("Test3")
+    assert_equal "......-...--.", @translator.eng_to_morse("heLP")
+    assert_equal "-....-...--", @translator.eng_to_morse("Test3")
+    assert_equal "-.....--.--...", @translator.eng_to_morse("BIG7")
   end
 
   def test_running_translate_from_file
-    translator = Translator.new
-    assert_equal ".. .--- ..-. .- ..-....-...", translator.from_file("./lib/input.txt")
+    assert_equal ".. .--- ..-. .- ..-....-...", @translator.from_file("./lib/input.txt")
+    assert_equal ".. .--- -..... ..-....-...", @translator.from_file("./lib/input2.txt")
   end
 
   def test_translate_morse_code_to_english
-    translator = Translator.new
-    assert_equal "he llo", translator.morse_to_eng(".... .  .-.. .-.. ---")
-    assert_equal "t3st1ng", translator.morse_to_eng("- ...-- ... - .---- -. --.")
-    assert_equal "sp   ace", translator.morse_to_eng("... .--.    .- -.-. .")
+    assert_equal "he llo", @translator.morse_to_eng(".... .  .-.. .-.. ---")
+    assert_equal "t3st1ng", @translator.morse_to_eng("- ...-- ... - .---- -. --.")
+    assert_equal "sp   ace", @translator.morse_to_eng("... .--.    .- -.-. .")
   end
 end
